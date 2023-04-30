dc4d3774744a04f2cf5b7abc65aac36305050fa3
Kris Shcheglova <kris.shcheglova@bk.ru>
2023-04-30 18:44:31 +0300

    feat: create basic html structure in js file

---
+++ script.js
@@ -0,0 +1,27 @@
+const body = document.querySelector('body'); // create basic html structure 
+
+// create container for project
+
+const section = document.createElement('section');
+section.className = 'container';
+
+// create title in project container
+const title = document.createElement('h2');
+title.className = 'title';
+title.innerText = 'RSS Виртуальная клавиатура';
+
+section.appendChild(title);
+
+// create title in project container
+const textarea = document.createElement('textarea');
+textarea.className = 'textarea';
+
+section.appendChild(textarea);
+
+// create container for keyboard
+const keyboard = document.createElement('div');
+keyboard.className = 'keyboard-container';
+
+// add everything in body
+section.appendChild(keyboard);
+body.appendChild(section);
